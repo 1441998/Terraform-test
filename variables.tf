63a6e437e82acc7286d37d8e35b9982672dc1cf7
Rubab <81675484+1441998@users.noreply.github.com>
2026-03-27 16:49:53 +0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_id" {
+  type        = string
+  description = "7d27d607ac0a4e759279c822d746399f"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "ai-test"
+}
