ce54f140c7cc02430ebe11af8d9e9b23babcb5a0
Rubab <81675484+1441998@users.noreply.github.com>
2026-03-27 17:58:25 +0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,16 @@
-variable "resource_group_id" {
+variable "resource_group_name" {
   type        = string
-  description = "7d27d607ac0a4e759279c822d746399f"
+  description = "The name of your resource group (e.g., Default)"
+  default     = "migration-dev"
+}
+
+variable "ssh_key_id" {
+  type        = string
+  description = "The ID of your SSH Key"
+  default     = "r006-fb7d06f3-039f-4753-a34a-0c5870c2e0f5"
 }
 
 variable "vpc_name" {
   type        = string
-  default     = "ai-test"
+  default     = "dallas-vpc"
 }
